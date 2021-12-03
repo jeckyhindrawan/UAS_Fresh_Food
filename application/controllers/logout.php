75f1421469f72b5be114a0ca08dc64afdec623e6
WilliamsTPL <91837394+WilliamsTPL@users.noreply.github.com>
2021-12-03 12:01:36 +0700

    Logout

---
+++ application/controllers/logout.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class logout extends CI_Controller
+{
+    public function index(){
+        $this->session->unset_userdata('id');
+        $this->session->unset_userdata('username');
+        redirect('login');
+    }
+}
