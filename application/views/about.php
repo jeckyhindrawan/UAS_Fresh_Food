1c5635c485ce471fd83f7af29dc0c9cb6c9ede82
Aldi Setiawan <aldisetiawan.0508@gmail.com>
2021-12-06 11:50:11 +0700

    git

---
+++ application/views/about.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Fresh Food | About</title>
+    <link rel="stylesheet" href="http://localhost/freshfood/assets/styles/home.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+</head>
+
+<body>
+    <div class="section">
+        <div class="container">
+            <div class="content-section">
+                <div class="title">
+                    <h1>About Us</h1>
+                </div>
+                <div class="content">
+                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                    <p> Modi iste velit doloribus totam, saepe
+                        voluptatem commodi ratione, ullam aliquam repellat quo. Quisquam cum veniam inventore
+                        exercitationem non hic voluptatem odio.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit corporis est maiores ullam ipsum
+                        architecto quod error quis hic pariatur, commodi vitae molestias quibusdam mollitia tempore
+                        dolores ea. Laboriosam, perspiciatis.
+                    </p>
+                </div>
+                <div class="button">
+                    <a href="">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
